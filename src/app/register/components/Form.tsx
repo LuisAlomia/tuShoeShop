e45bed04dde54330985b6958b9f09092d3e512c5
Luis Alomia <luisfidel44@gmail.com>
2023-05-24 11:39:12 -0500

    feat(register): add pages and component Form

---
+++ src/app/register/components/Form.tsx
@@ -0,0 +1,102 @@
+"use client";
+
+import Link from "next/link";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  username: string;
+  email: string;
+  password: string;
+};
+
+const Form = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const submit: SubmitHandler<Inputs> = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(submit)} className="p-4 ">
+      <div>
+        <input
+          type="text"
+          placeholder="Usuario"
+          className="input"
+          {...register("username", {
+            required: "El campo usuario es requerido",
+          })}
+        />
+        {errors.username && (
+          <p className="text-red-500 text-xs" role="alert">
+            {errors.username?.message}
+          </p>
+        )}
+      </div>
+
+      <div>
+        <input
+          type="text"
+          placeholder="Correo"
+          className="input"
+          {...register("email", {
+            required: "El campo correo es requerido",
+          })}
+        />
+        {errors.email && (
+          <p className="text-red-500 text-xs" role="alert">
+            {errors.email?.message}
+          </p>
+        )}
+      </div>
+
+      <div>
+        <input
+          type="password"
+          placeholder="Contraseña"
+          className="input"
+          {...register("password", {
+            required: "El campo contraseña es requerido",
+          })}
+        />
+        {errors.password && (
+          <p className="text-red-500 text-xs" role="alert">
+            {errors.password?.message}
+          </p>
+        )}
+      </div>
+
+      <p className="text-slate-500 mt-10 text-sm">
+        ¿Ya tienes una cuenta?{" "}
+        <Link
+          href={"/login"}
+          className="text-black hover:text-slate-600 cursor-pointer"
+        >
+          Ingresa ahora.
+        </Link>
+      </p>
+
+      {/* {resultFech === 404 ||
+            (active === false && (
+              <p className="pl-1 text-xs text-rose-500">
+                el usuario no se encuentra registrado
+              </p>
+            ))}
+          {resultFech === 400 && (
+            <p className="pl-1 text-xs text-rose-500">
+              el usuario o contraseña son incorrectos
+            </p>
+          )} */}
+
+      <button className="w-full bg-black hover:bg-[#2b2b2b] p-3 rounded-lg border-transparent mt-8 text-white uppercase">
+        Registrar
+      </button>
+    </form>
+  );
+};
+
+export default Form;
---
+++ src/app/register/page.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import Form from "./components/Form";
+import arrowSvg from "../../../public/svg/arrow.svg";
+import Image from "next/image";
+
+const Register = () => {
+  return (
+    <main className="h-screen flex-col lg:flex lg:flex-row">
+      <section className="lg:w-full lg:h-screen image-login"></section>
+      <section
+        className={`hero w-full flex flex-shrink-0 h-full flex-col justify-center lg:w-1/3 lg:bg-[#ebebeb]`}
+      >
+        <h4 className="text-4xl px-[2rem] cursor-default font-bold text-slate-500 relative">
+          <button className="flex justify-center items-center rounded-full bg-white hover:bg-[#dddddd] w-12 h-12 p-1 ml-[-2.5rem] cursor-pointer absolute left-[2rem] top-[-2rem] shadow-md">
+            <Link href={"/"} className="">
+              <Image src={arrowSvg} alt="arrow" className="w-8" />
+            </Link>
+          </button>
+          <p>Descubre nuevos estilos.</p>
+          <span>
+            <span className="text-black text-5xl">Registrate </span>ahora!
+          </span>
+        </h4>
+        <div className="form">
+          <Form />
+        </div>
+      </section>
+    </main>
+  );
+};
+
+export default Register;
