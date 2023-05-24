771871ce682825e841dfd32b979b4788493bbe93
Luis Alomia <luisfidel44@gmail.com>
2023-05-24 11:40:34 -0500

    feat(data): add data of products and categories

---
+++ src/data/categories.data.ts
@@ -0,0 +1,12 @@
+import { Category, Genre } from "@/types/category.type";
+
+export const Categories: Category[] = [
+  {
+    id: "1",
+    name: Genre.hombre,
+  },
+  {
+    id: "2",
+    name: Genre.mujer,
+  },
+];
---
+++ src/data/products.data.ts
@@ -0,0 +1,141 @@
+import { Genre } from "@/types/category.type";
+import { Product } from "@/types/product.type";
+
+export const products: Product[] = [
+  {
+    id: "1",
+    name: "Nike ZoomX Vaporfly",
+    image: [
+      "https://i.ibb.co/drCn3zV/20.jpg",
+      "https://i.ibb.co/hVg9m7H/5-removebg-preview.png",
+      "https://i.ibb.co/wpCZTFW/4-removebg-preview.png",
+      "https://i.ibb.co/M8bBtrV/3-removebg-preview.png",
+      "https://i.ibb.co/b36P0cK/2-removebg-preview.png",
+    ],
+    price: 100,
+    category: {
+      id: "1",
+      name: Genre.hombre,
+    },
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima enim distinctio ex illo porro qui odit animi sint! Suscipit aliquid sunt a eaque repellendus ipsam possimus! Quaerat et cumque nihil! Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, quia. Dolores natus voluptas ut reiciendis distinctio reprehenderit ducimus fugit laborum, nobis veritatis fuga provident unde. Natus id eius harum consectetur.",
+  },
+  {
+    id: "2",
+    name: "Sion 1 SP",
+    image: [
+      "https://i.ibb.co/X2K6pnc/2-removebg-preview.png",
+      "https://i.ibb.co/1JgMKsv/1-removebg-preview.png",
+    ],
+    price: 300,
+    category: {
+      id: "1",
+      name: Genre.hombre,
+    },
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima enim distinctio ex illo porro qui odit animi sint! Suscipit aliquid sunt a eaque repellendus ipsam possimus! Quaerat et cumque nihil! Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, quia. Dolores natus voluptas ut reiciendis distinctio reprehenderit ducimus fugit laborum, nobis veritatis fuga provident unde. Natus id eius harum consectetur.",
+  },
+  {
+    id: "3",
+    name: "Nike pegasus trial 3 Gore-Tex",
+    image: [
+      "https://i.ibb.co/wMHHHFy/8.jpg",
+      "https://i.ibb.co/yBTXGcZ/5-removebg-preview.png",
+      "https://i.ibb.co/b2VCJqc/4-removebg-preview.png",
+      "https://i.ibb.co/LJqxHyR/3-removebg-preview.png",
+      "https://i.ibb.co/svqS3zr/2-removebg-preview.png",
+    ],
+    price: 100,
+    category: {
+      id: "1",
+      name: Genre.mujer,
+    },
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima enim distinctio ex illo porro qui odit animi sint! Suscipit aliquid sunt a eaque repellendus ipsam possimus! Quaerat et cumque nihil! Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, quia. Dolores natus voluptas ut reiciendis distinctio reprehenderit ducimus fugit laborum, nobis veritatis fuga provident unde. Natus id eius harum consectetur.",
+  },
+  {
+    id: "4",
+    name: "Nike court zoom NXT",
+    image: [
+      "https://i.ibb.co/Rg5ZbKp/17.jpg",
+      "https://i.ibb.co/3Yw9Df0/4-removebg-preview.png",
+      "https://i.ibb.co/m8w64Gp/3-removebg-preview.png",
+      "https://i.ibb.co/s1KXDsT/2-removebg-preview.png",
+    ],
+    price: 100,
+    category: {
+      id: "1",
+      name: Genre.mujer,
+    },
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima enim distinctio ex illo porro qui odit animi sint! Suscipit aliquid sunt a eaque repellendus ipsam possimus! Quaerat et cumque nihil! Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, quia. Dolores natus voluptas ut reiciendis distinctio reprehenderit ducimus fugit laborum, nobis veritatis fuga provident unde. Natus id eius harum consectetur.",
+  },
+  {
+    id: "5",
+    name: "Nike Air vaporMax Plus",
+    image: [
+      "https://i.ibb.co/DpvK7d3/15.jpg",
+      "https://i.ibb.co/4Frp8xx/4-removebg-preview.png",
+      "https://i.ibb.co/ZgVgQX7/3-removebg-preview.png",
+      "https://i.ibb.co/fYLM9jn/2-removebg-preview.png",
+    ],
+    price: 100,
+    category: {
+      id: "1",
+      name: Genre.mujer,
+    },
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima enim distinctio ex illo porro qui odit animi sint! Suscipit aliquid sunt a eaque repellendus ipsam possimus! Quaerat et cumque nihil! Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, quia. Dolores natus voluptas ut reiciendis distinctio reprehenderit ducimus fugit laborum, nobis veritatis fuga provident unde. Natus id eius harum consectetur.",
+  },
+  {
+    id: "6",
+    name: "Nike zoomX Invincible Run Flyknit",
+    image: [
+      "https://i.ibb.co/bzrgHqx/10.jpg",
+      "https://i.ibb.co/85KLsNh/5-removebg-preview.png",
+      "https://i.ibb.co/ZY95t04/4-removebg-preview.png",
+      "https://i.ibb.co/c8jJ6Tg/3-removebg-preview.png",
+      "https://i.ibb.co/smjy2Cd/2-removebg-preview.png",
+    ],
+    price: 100,
+    category: {
+      id: "1",
+      name: Genre.hombre,
+    },
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima enim distinctio ex illo porro qui odit animi sint! Suscipit aliquid sunt a eaque repellendus ipsam possimus! Quaerat et cumque nihil! Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, quia. Dolores natus voluptas ut reiciendis distinctio reprehenderit ducimus fugit laborum, nobis veritatis fuga provident unde. Natus id eius harum consectetur.",
+  },
+  {
+    id: "7",
+    name: "Nike Air Force 1 React",
+    image: [
+      "https://i.ibb.co/qywFWgs/5.jpg",
+      "https://i.ibb.co/98kHBq7/3-removebg-preview.png",
+      "https://i.ibb.co/7yf6rNT/2-removebg-preview.png",
+    ],
+    price: 100,
+    category: {
+      id: "1",
+      name: Genre.hombre,
+    },
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima enim distinctio ex illo porro qui odit animi sint! Suscipit aliquid sunt a eaque repellendus ipsam possimus! Quaerat et cumque nihil! Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, quia. Dolores natus voluptas ut reiciendis distinctio reprehenderit ducimus fugit laborum, nobis veritatis fuga provident unde. Natus id eius harum consectetur.",
+  },
+  {
+    id: "8",
+    name: "Kyrie Flytrap 5",
+    image: [
+      "https://i.ibb.co/YTWdh5p/3.jpg",
+      "https://i.ibb.co/Ctyxf0q/4-removebg-preview.png",
+      "https://i.ibb.co/MC6PsNt/3-removebg-preview.png",
+      "https://i.ibb.co/JzZ9hRC/2-removebg-preview.png",
+    ],
+    price: 100,
+    category: {
+      id: "1",
+      name: Genre.hombre,
+    },
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima enim distinctio ex illo porro qui odit animi sint! Suscipit aliquid sunt a eaque repellendus ipsam possimus! Quaerat et cumque nihil! Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, quia. Dolores natus voluptas ut reiciendis distinctio reprehenderit ducimus fugit laborum, nobis veritatis fuga provident unde. Natus id eius harum consectetur.",
+  },
+];
